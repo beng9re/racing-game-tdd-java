ac3f3cd706093a422f053122a367bcd0a2adb0da
kbh0581 <kbh0581@gmail.com>
2021-10-11 02:23:21 +0900

    feat : 자동차가 주어진 횟수(사용자 시도 횟수) 동안 각 자동차경기를 수행을 하는 기능 추가

---
+++ src/main/java/racinggame/RacingGameService.java
@@ -0,0 +1,24 @@
+package racinggame;
+
+import nextstep.utils.Randoms;
+import racinggame.domain.Cars;
+
+
+public class RacingGameService {
+
+	public Cars carsCreate(String carNames) {
+		return new Cars(carNames);
+	}
+
+	public String racing(Cars cars) {
+		StringBuilder stringBuilder = new StringBuilder();
+		for (int i = 0; i < cars.size(); i++) {
+			cars.getCar(i).racing(Randoms.pickNumberInRange(0, 9));
+			stringBuilder.append(cars.getCar(i));
+			stringBuilder.append("\n");
+		}
+		return stringBuilder.toString();
+	}
+
+
+}
