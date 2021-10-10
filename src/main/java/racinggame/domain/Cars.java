287fa8111aab1dde03531436f66864d85f39faf9
kbh0581 <kbh0581@gmail.com>
2021-10-10 13:57:58 +0900

    feat : 자동차들을 문자열로 받아 생성하는 기능
    - 쉼표 유효성검사
    - 해당역할을 담당하는 Cars 일급 컬랙션 생성

---
+++ src/main/java/racinggame/domain/Cars.java
@@ -0,0 +1,39 @@
+package racinggame.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import racinggame.util.ValidationUtils;
+
+public class Cars {
+	List<Car> carList;
+	private static final String REGEX_TOKEN = ",";
+
+	Cars(String carNames) {
+		validation(carNames);
+		carList = mapCarList(carNames);
+	}
+
+	private List<Car> mapCarList(String carNames) {
+		List<Car> carList = new ArrayList<>();
+		for (String carName : carNames.split(REGEX_TOKEN)) {
+			carList.add(new Car(carName));
+		}
+		return carList;
+	}
+
+	public Car getCar(int index) {
+		return carList.get(index);
+	}
+
+
+	public int size() {
+		return this.carList.size();
+	}
+
+	private void validation(String carNames) {
+		if(!ValidationUtils.nameValid(carNames)) {
+			throw new IllegalArgumentException("[ERROR] 자동차 이름은 쉼표(,)로 구별하여야 합니다.");
+		}
+	}
+}
---
+++ src/test/java/racinggame/domain/CarsTest.java
@@ -0,0 +1,38 @@
+package racinggame.domain;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class CarsTest {
+
+
+	@Test
+	void 쉼표로_구분으로_문자열을_받아_자동차들을_생성한다() {
+		String carNameString = "sads,sad,asdr,fgv,asdd";
+		String[] carsName = carNameString.split(",");
+		Cars cars = new Cars(carNameString);
+		assertThat(cars).isNotNull();
+		assertThat(cars.size() == carsName.length).isTrue();
+		for (int i = 0; i < cars.size(); i++) {
+			assertThat(cars).isNotNull();
+			assertThat(cars.getCar(i).carName()).isEqualTo(carsName[i]);
+		}
+	}
+
+	@ParameterizedTest
+	@ValueSource(strings = {
+		"sads,sad,asdr,fgvs,asdd,",
+		"sads,sad,asdr,fgv,asddssa",
+		"sads,sad,asdr,fgv,,",
+		"sads,sad,,asdr,fgv,"
+	})
+	void 잘못된_차이름_유효성검사(String carNames) {
+		assertThatIllegalArgumentException()
+			.isThrownBy(() -> new Cars(carNames));
+	}
+
+
+}
